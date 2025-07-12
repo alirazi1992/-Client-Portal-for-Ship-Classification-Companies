91450e51fc4272be38ce47190c59863ef4bd21fa
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-07-12 10:33:33 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './hooks/useAuth';
+import Layout from './components/Layout';
+
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Certificates from './pages/Certificates';
+import SupportTicketRequest from './pages/InspectionRequest';
+import SurveyStatus from './pages/SurveyStatus';
+import EngineerDashboard from './pages/EngineerDashboard';
+import AssignedEngineer from './pages/AssignedEngineer';
 
 const App = () => {
   const { user, loading } = useAuth();
@@ -21,12 +29,59 @@ const App = () => {
         {!user ? (
           <>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="*" element={<Navigate to="/login" replace />} />
           </>
         ) : (
           <>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Navigate to="/" replace />} />
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Dashboard />
+                </Layout>
+              }
+            />
+            <Route
+              path="/certificates"
+              element={
+                <Layout>
+                  <Certificates />
+                </Layout>
+              }
+            />
+            <Route
+              path="/inspection-request"
+              element={
+                <Layout>
+                  <SupportTicketRequest />
+                </Layout>
+              }
+            />
+            <Route
+              path="/status"
+              element={
+                <Layout>
+                  <SurveyStatus />
+                </Layout>
+              }
+            />
+            <Route
+              path="/engineer-dashboard/:name"
+              element={
+                <Layout>
+                  <EngineerDashboard />
+                </Layout>
+              }
+            />
+            <Route
+              path="/assigned/:id"
+              element={
+                <Layout>
+                  <AssignedEngineer />
+                </Layout>
+              }
+            />
             <Route path="*" element={<Navigate to="/" replace />} />
           </>
         )}
